87d819e3d443f527901cdec5a60a34e91fca64b2
ready4exam <ready4urexam@gmail.com>
2025-12-17 20:34:52 +0530

    chore: update table_id for Statistics → mathematics_statistics_statistics_10_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -282,7 +282,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Statistics",
-        "table_id": "",
+        "table_id": "mathematics_statistics_statistics_10_quiz",
         "section": "Mathematics"
       },
       {
